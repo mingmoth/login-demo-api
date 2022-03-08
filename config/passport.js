006cbd21109f9985b6481082a2409e9807f7a7dc
mingmoth <mingmoth@gmail.com>
2022-03-08 12:04:27 +0800

    modify: google callback url

--- config/passport.js
+++ config/passport.js
@@ -57,7 +57,7 @@ passport.use(new FacebookStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/api/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CLIENT_CALLBACK || "http://localhost:3000/api/auth/google/callback"
 },
   function (accessToken, refreshToken, profile, cb) {
     const { name, email, picture } = profile._json
